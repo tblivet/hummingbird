49cc05680e59117bd2e7e5b0a9f4a909cbdb785b
tblivet <thibault.blivet@prestashop.com>
2025-07-18 17:41:41 +0200

    feat: improve checkout compatibility

--- src/js/theme.ts
+++ src/js/theme.ts
@@ -32,6 +32,7 @@ import initScrollPaddingTop from '@helpers/scrollPadding';
 import initProductAccessibility from '@js/accessibility/product';
 import initCartAccessibility from '@js/accessibility/cart';
 import initProductComments from '@js/modules/productcomments';
+import './modules/ps_checkout';
 
 initEmitter();
 
